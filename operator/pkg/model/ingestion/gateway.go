9c548932549fffc8bd9f51e608e148c5fcf4e403
Andrew Sauber <andrew.sauber@isovalent.com>
2026-06-10 12:02:37 +0900

    fixup! gatewayapi: add ListenerSet ingestion and route filtering

--- operator/pkg/model/ingestion/gateway.go
+++ operator/pkg/model/ingestion/gateway.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
