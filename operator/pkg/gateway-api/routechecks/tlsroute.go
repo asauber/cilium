277d2040db813aeb757ef5cfeb1f8636fd0322d0
Andrew Sauber <andrew.sauber@isovalent.com>
2026-06-10 12:02:37 +0900

    fixup! gatewayapi: update route filtering for ListenerSet sources

--- operator/pkg/gateway-api/routechecks/tlsroute.go
+++ operator/pkg/gateway-api/routechecks/tlsroute.go
@@ -114,7 +114,7 @@ func (t *TLSRouteInput) GetHostnames() []gatewayv1.Hostname {
 	return t.TLSRoute.Spec.Hostnames
 }
 
-func (t *TLSRouteInput) GetListenerOwner(parent gatewayv1.ParentReference) (ListenerOwner, error) {
+func (t *TLSRouteInput) GetListenerSource(parent gatewayv1.ParentReference) (ListenerSource, error) {
 	if t.gateways == nil {
 		t.gateways = make(map[gatewayv1.ParentReference]ListenerOwner)
 	}
